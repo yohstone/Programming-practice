73785f1dc88ce90fa59e40a8e9b7bf5ca8d6eaea
YJL <steve_stone@126.com>
2019-05-31 23:26:29 +0800

    leetcode #292

---
+++ leetcode-292-nim game.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//You are playing the following Nim Game with your friend: There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones.
+//The one who removes the last stone will be the winner. You will take the first turn to remove the stones.
+//Both of you are very clever and have optimal strategies for the game. Write a function to determine whether you can win the game given the number of stones in the heap.
+class Solution{
+public:
+    bool canWinNim(int n){
+        if( n % 4 == 0) return false;
+        else return true;
+    }
+};
+int main(){
+    Solution s;
+    int n;
+    while(cin >> n){
+       cout << s.canWinNim(n) << endl;
+    }
+    return 0;
+}
