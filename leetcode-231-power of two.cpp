9ee43f056e21a4cad38792f61ce0bc5d4fc2f8bc
YJL <steve_stone@126.com>
2019-06-01 23:21:08 +0800

    leetcode #231

---
+++ leetcode-231-power of two.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+//Given an integer, write a function to determine if it is a power of two.
+//
+//Example 1:
+//
+//Input: 1
+//Output: true
+//Explanation: 20 = 1
+//Example 2:
+//
+//Input: 16
+//Output: true
+//Explanation: 24 = 16
+
+class Solution{
+public:
+    bool isPowerOfTwo(int n){
+        if( n > 0 && ( n & (n-1)) == 0  )
+            return true;
+        else
+            return false;
+    }
+};
+
+int main(){
+    Solution s;
+    cout << s.isPowerOfTwo(16) <<endl;
+    return 0;
+}
