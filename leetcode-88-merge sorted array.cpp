ebc5904f2f5be987131868ab9fb607933ed63f64
YJL <steve_stone@126.com>
2019-06-04 23:07:09 +0800

    update leetcode #88

--- leetcode-88-merge sorted array.cpp
+++ leetcode-88-merge sorted array.cpp
@@ -21,14 +21,14 @@ class Solution{
 public:
     // 从num1尾部开始合并
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
-        int p = m + n - 1;
+        int p = m + n- 1;
         int p1 = m-1, p2 = n-1;
         if(m == 0){
             nums1.swap(nums2);
             return ;
         }
         while( p >= 0 && p2 >= 0 ){
-            if( nums1[p1] > nums2[p2] ){
+            if( p1 >= 0 && nums1[p1] > nums2[p2] ){
                 nums1[p] = nums1[p1];
                 nums1[p1] = 0;
                 p1--;
