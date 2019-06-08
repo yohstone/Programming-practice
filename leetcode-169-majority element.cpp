055ffa438e4f7b1da04eb693c6b393128fc3ee5b
YJL <steve_stone@126.com>
2019-06-08 13:37:07 +0800

    update leetcode #169

--- leetcode-169-majority element.cpp
+++ leetcode-169-majority element.cpp
@@ -4,6 +4,22 @@
 #include<algorithm>
 #include<stack>
 using namespace std;
+
+//Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+//
+//You may assume that the array is non-empty and the majority element always exist in the array.
+//
+//Example 1:
+//
+//Input: [3,2,3]
+//Output: 3
+//Example 2:
+//
+//Input: [2,2,1,1,1,2,2]
+//Output: 2
+
+
+
 class Solution{
 public:
     // 方法1 先排序 后取中间值
