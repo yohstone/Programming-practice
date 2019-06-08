e044badeef99565e348a57355541a957ff28af36
YJL <steve_stone@126.com>
2019-06-08 11:25:15 +0800

    update #169

--- leetcode-169-majority element.cpp
+++ leetcode-169-majority element.cpp
@@ -12,26 +12,61 @@ public:
         return nums[ nums.size()/2 ];
 
     }
-    // 方法2 栈
+    // 方法2 栈 栈为空或等于栈顶入栈，不等于栈顶时出栈
     int majorityElement2(vector<int>& nums){
+        stack<int> st;
+        for(int i=0; i<nums.size(); i++){
+            if(st.empty() || st.top() == nums[i]){
+                st.push(nums[i]);
 
-
+            }else{
+                st.pop();
+            }
+        }
+        return st.top();
     }
 
-    // 方法3 计数抵消
+    // 方法3 计数抵消 count初始为1，currNum为第一个数，当下一个组等于currNum时count+1，不相等时count-1，count=0时更换currNum为当前数，最终的currNum则为众数
     int majorityElement3(vector<int>& nums){
-
+        int count=1;
+        int currNum = nums[0];
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] == currNum)
+                count++;
+            else{
+                count--;
+                if(count == 0){
+                    currNum = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return currNum;
 
     }
-    // 方法4 map
+    // 方法4 map 利用map的关联容器以及key唯一的特性， key为nums里的每个数，value为每个数出现的次数
     int majorityElement4(vector<int>& nums){
-
-
+        map<int, int> numsMap;
+        // 统计各数出现次数
+        for(int i=0; i<nums.size(); i++){
+            numsMap[nums[i]] ++;
+        }
+        map<int, int>::iterator iter;
+        int maxCountNum = 0, count = 0;
+        // 寻找众数
+        for(iter = numsMap.begin(); iter != numsMap.end(); iter++){
+            //cout<< iter->first << " - " << iter->second <<endl;
+            if(iter->second > count){
+                maxCountNum = iter->first;
+                count = iter->second;
+            }
+        }
+        return maxCountNum;
     }
 };
 int main(){
     Solution s;
     vector<int> nums = {2,2,1,1,1,2,2};
-    cout << s.majorityElement1(nums) << endl;
+    cout << s.majorityElement4(nums) << endl;
     return 0;
 }
