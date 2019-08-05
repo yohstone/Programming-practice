4005961d2a0e7056cae5cbebdf53f79eae32992c
yjl <steve_stone@126.com>
2019-08-05 14:46:53 +0800

    old file but code is newer

--- leetcode-500-keyboard row.cpp
+++ leetcode-500-keyboard row.cpp
@@ -1,25 +1,46 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
+//Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
+//
+//Example:
+//
+//Input: ["Hello", "Alaska", "Dad", "Peace"]
+//Output: ["Alaska", "Dad"]
+
 class Solution {
 public:
     vector<string> findWords(vector<string>& words) {
         vector<string> keybord = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
         vector<string> res;
-        int flag = 0;
+        int kLine = 0;    // 标记当前单词由第几行键盘打印, 值为-1时表示不能同一行打印
         for(int i=0; i<words.size(); i++){
-                string temp = words[i];
+            string temp = words[i]; // 便于转换大小写，同时不改变原单词
+            
             for(int j=0; j<temp.length(); j++){
+                // 单词字母转成小写
                 if( temp[j]>='A' && temp[j] <= 'Z'){
                     temp[j] += 32; // 转为小写
                 }
-                if( keybord[0].find( temp[j] ) != -1 || keybord[1].find( temp[j] ) != -1 || keybord[2].find( temp[j] ) != -1){
-                    flag = 1;
+                // 先判断首字母属于键盘第几行
+                if( j == 0 ){
+                    if( keybord[0].find( temp[j] ) != -1 )
+                        kLine = 0;
+                    else if( keybord[1].find( temp[j] )  != -1 )
+                        kLine = 1;
+                    else if( keybord[2].find( temp[j] )  != -1 )
+                        kLine = 2;
                 }else{
-                    flag = 0;
+                    // 若有一个字母和首字母不在同一行，则该单词非所求
+                    if( keybord[ kLine ].find( temp[j] ) == -1 ){
+                        kLine = -1;
+                        break;
+                    }
                 }
+                
             }
-            if( flag == 1){
+            if( kLine != -1){
                 res.push_back(words[i]);
             }
         }
