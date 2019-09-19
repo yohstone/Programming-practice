27e48b11a515b9829f382a024f97462df5c7b1c8
YJL <steve_stone@126.com>
2019-09-19 18:53:20 +0800

    add new method and update comment

--- leetcode-232-implement queue using stacks.cpp
+++ leetcode-232-implement queue using stacks.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <stack>
 #include <algorithm>
+#include <exception>
 using namespace std;
 
 /* Implement the following operations of a queue using stacks.
@@ -21,7 +22,8 @@ using namespace std;
  * queue.empty(); // returns false
  */
 
-// 方法， 两个栈实现
+// 方法1， 两个栈实现，一个栈1 s 用于存储数据，另一个栈2 tmp 用于弹出数据时的临时容器。
+// 实现 queue 中的弹出操作时，先把栈底元素上面的元素暂时放入 tmp ，然后弹出栈底元素，最后把 tmp 中的元素重新压入 s。
 class MyQueue {
 public:
     /** Initialize your data structure here. */
@@ -33,7 +35,7 @@ public:
     void push(int x) {
         // 栈底元素为队列的首部元素
         if(s.size() == 0){
-            frontValue = x;
+            frontElem = x;
         }
         s.push(x);
     }
@@ -41,7 +43,7 @@ public:
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
         int sSize = s.size();
-        int res = frontValue;
+        int res = frontElem;
         // 将栈底以上的元素暂放入另一个栈中
         while(sSize > 1){
             tmp.push( s.top() );
@@ -53,7 +55,7 @@ public:
         // 将元素放回栈
         while(tmp.size() > 0){
             if(s.size() == 0){
-                frontValue = tmp.top();
+                frontElem = tmp.top();
             }
             s.push( tmp.top() );
             tmp.pop();
@@ -63,7 +65,7 @@ public:
 
     /** Get the front element. */
     int peek() {
-        return frontValue;
+        return frontElem;
     }
 
     /** Returns whether the queue is empty. */
@@ -73,10 +75,131 @@ public:
     }
 private:
     stack<int> s, tmp;
-    int frontValue;
+    int frontElem;
 };
 
 
+// 方法2， 两个栈实现，一个栈 stack1 用于压入数据，另一个栈 stack2 用于弹出数据。
+// 实现 queue 中的弹出操作时，先判断 stack2 是否为空，若为空，将 stack1 的数据压入 stack2，
+// 当 stack2 不为空时，直接弹出 stack2 的栈顶元素。
+// 此方法较方法1 省略了在弹出元素之后将数据重新压回栈1的操作，同时两个栈都可以用于存储数据，空间更大。
+class MyQueue2 {
+public:
+    /** Initialize your data structure here. */
+    MyQueue2() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        if(stack1.size() == 0){         // 栈1为空时，记录下栈1的栈底元素
+            stack1Base = x;
+            if(stack2.size() == 0){     // 栈2也为空，则 x 为队列首部元素
+                frontElem = x;
+            }
+        }
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int res = frontElem;
+        if(stack2.size() <= 0){
+            // 将栈1 stack1 中的元素压入 stack2，这样顺序就倒过来了
+            while(stack1.size() > 0){
+                stack2.push( stack1.top() );
+                stack1.pop();
+            }
+            stack1Base = -1;  // 更新 stack1 的栈底元素
+        }
+//        if(stack2.size() == 0){
+//            throw new exception("queue is empty!");
+//        }
+        stack2.pop();
+        if(stack2.size() > 0){          // 更新队列首元素的值
+            frontElem = stack2.top();
+        }else{
+            frontElem = stack1Base;
+        }
+        return res;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        return frontElem;
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        if(stack1.size() == 0 && stack2.size() == 0) return true;
+        else return false;
+    }
+private:
+    int frontElem;    // 队首元素
+    int stack1Base;   // stack1 的栈底元素
+    stack<int> stack1, stack2;
+};
+
+
+// 方法2 的模板实现
+template<typename T>
+class MyQueue3 {
+private:
+    T frontElem;    // 队首元素
+    T stack1Base;   // stack1 的栈底元素
+    stack<T> stack1, stack2;
+public:
+    /** Initialize your data structure here. */
+    MyQueue3() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    void push(T x) {
+        if(stack1.size() == 0){         // 栈1为空时，记录下栈1的栈底元素
+            stack1Base = x;
+            if(stack2.size() == 0){     // 栈2也为空，则 x 为队列首部元素
+                frontElem = x;
+            }
+        }
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    T pop() {
+        T res = frontElem;
+        if(stack2.size() <= 0){
+            // 将栈1 stack1 中的元素压入 stack2，这样顺序就倒过来了
+            while(stack1.size() > 0){
+                stack2.push( stack1.top() );
+                stack1.pop();
+            }
+            stack1Base = -1;  // 更新 stack1 的栈底元素
+        }
+//        if(stack2.size() == 0){
+//            throw new exception("queue is empty!");
+//        }
+        stack2.pop();
+        if(stack2.size() > 0){          // 更新队列首元素的值
+            frontElem = stack2.top();
+        }else{
+            frontElem = stack1Base;
+        }
+        return res;
+    }
+
+    /** Get the front element. */
+    T peek() {
+        return frontElem;
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        if(stack1.size() == 0 && stack2.size() == 0) return true;
+        else return false;
+    }
+
+};
 
 
 int main(){
