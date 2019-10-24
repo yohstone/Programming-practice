e83626edad8bbe5e99e37b48a572e54e724035c3
YJL <steve_stone@126.com>
2019-10-24 12:15:36 +0800

    add new method to leetcode #111

--- leetcode/leetcode-111-minimum depth of binary tree.cpp
+++ leetcode/leetcode-111-minimum depth of binary tree.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<queue>
+#include<stack>
 #include<vector>
 using namespace std;
 
@@ -49,7 +49,7 @@ struct TreeNode {
 
 class Solution {
 public:
-    // 方法1：递归法
+    // 方法1：递归法，深度优先遍历DFS
     int minDepth(TreeNode* root){
         if (root == nullptr)
             return 0;
@@ -61,6 +61,73 @@ public:
         }
         return min(leftDepth, rightDepth) + 1;    // 同时存在左右子树
     }
+
+    // 方法2：迭代+深度优先遍历DFS
+    int minDepth2(TreeNode* root){
+        if(root == nullptr){
+            return 0;
+        }
+        stack<pair<TreeNode*, int>> treeStack; // 存储树的结点及其对应的深度
+        treeStack.push(make_pair(root, 1));
+        int min_depth = INT_MAX;
+        while(!treeStack.empty()){
+            TreeNode* currNode = treeStack.top().first;
+            int currDepth = treeStack.top().second;
+            treeStack.pop();
+            if(currNode->left == nullptr && currNode->right == nullptr){ // 遇到叶子结点，更新最小深度
+                min_depth = min(min_depth, currDepth);
+            }
+            if(currNode->left){
+                treeStack.push(make_pair(currNode->left, currDepth + 1));
+            }
+            if(currNode->right){
+                treeStack.push(make_pair(currNode->right, currDepth + 1));
+            }
+        }
+        return min_depth;
+    }
+    // 方法3：递归+广度优先遍历 BFS，遇到叶子结点返回，遇到非叶子结点则递归，每递归一次 depth + 1
+    int minDepth3(TreeNode* root){
+        if(root == nullptr){
+            return 0;
+        }
+        int depth = 1;
+        if(!root->left && !root->right){
+            return depth;
+        }
+        if(!root->left){
+            return depth + minDepth3(root->right);
+        }
+        if(!root->right){
+            return depth + minDepth3(root->left);
+        }
+        return depth + min(minDepth3(root->left), minDepth3(root->right));
+    }
+
+    // 方法4：迭代+广度优先遍历，将每一层结点压入队列中，若遇到叶子结点，则说明找到了最小的深度，直接返回层数
+    int minDepth4(TreeNode* root){
+        if(root == nullptr){
+            return 0;
+        }
+        queue<pair<TreeNode*, int>> treeQue; // 存储树的结点及其对应的深度
+        treeQue.push(make_pair(root, 1));
+        while(!treeQue.empty()){
+            TreeNode* currNode = treeQue.front().first;
+            int currDepth = treeQue.front().second;
+            treeQue.pop();
+            if(currNode->left == nullptr && currNode->right == nullptr){ // 遇到叶子结点，更新最小深度
+                return currDepth;
+            }
+            if(currNode->left){
+                treeQue.push(make_pair(currNode->left, currDepth + 1));
+            }
+            if(currNode->right){
+                treeQue.push(make_pair(currNode->right, currDepth + 1));
+            }
+        }
+        return 1;
+    }
+
 };
 int main(){
 
