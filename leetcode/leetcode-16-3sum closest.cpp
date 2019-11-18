b0aa993b8b57698245552a6a1961ed6da1871d67
YJL <steve_stone@126.com>
2019-11-18 17:00:57 +0800

    review and update comment

--- leetcode/leetcode-16-3sum closest.cpp
+++ leetcode/leetcode-16-3sum closest.cpp
@@ -12,10 +12,18 @@ using namespace std;
  * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
  */
 
+//    给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+//
+//    例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+//
+//    与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+
+
+
 #define INFINITY 65535       // 定义无穷大
 class Solution {
 public:
-
+    // 方法1：三指针法，先固定一个指针，然后使用双指针遍历接下来的数字，当双指针相遇时改变固定的那个指针
     int threeSumClosest(vector<int>& nums, int target) {
         int len = nums.size();
         sort(nums.begin(), nums.end());                         // 先对 nums 进行排序
