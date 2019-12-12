cadc8ab132ed9777f9a5e63999811e4a06b7db9b
YJL <steve_stone@126.com>
2019-12-12 17:24:05 +0800

    update comment

--- leetcode/leetcode-217-contains duplicate.cpp
+++ leetcode/leetcode-217-contains duplicate.cpp
@@ -1,26 +1,49 @@
 #include<iostream>
 #include<vector>
 #include<set>
+#include
 using namespace std;
 
-//Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+//    给定一个整数数组，判断是否存在重复元素。
 //
-//Example 1:
+//    如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
 //
-//Input: [1,2,3,1]
-//Output: true
-//Example 2:
+//    示例 1:
 //
-//Input: [1,2,3,4]
-//Output: false
-
+//    输入: [1,2,3,1]
+//    输出: true
+//    示例 2:
+//
+//    输入: [1,2,3,4]
+//    输出: false
+//    示例 3:
+//
+//    输入: [1,1,1,3,3,4,3,2,4,2]
+//    输出: true
+//
+//    Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
 
 class Solution {
 public:
+    // 方法1 ： 使用 set 的不重复性质，将 nums 中的元素复制到 set 中，若容器中元素个数不变，则不含重复元素
     bool containsDuplicate(vector<int>& nums) {
         set<int> numsSet( nums.begin(), nums.end() );
         return numsSet.size() != nums.size();
     }
+
+    // 方法2：先排序，然后判断相邻元素是否相等
+    bool containsDuplicate(vector<int>& nums) {
+        int len = nums.size();
+        if(len == 0)
+            return false;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < len - 1; ++i){
+            if(nums[i] == nums[i + 1])
+                return true;
+        }
+        return false;
+    }
+
 };
 int main(){
     Solution s;
--- leetcode/leetcode-53-maximum subarray.cpp
+++ leetcode/leetcode-53-maximum subarray.cpp
@@ -2,31 +2,52 @@
 #include<vector>
 using namespace std;
 
-//Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+//    给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
 //
-//Example:
+//    示例:
 //
-//Input: [-2,1,-3,4,-1,2,1,-5,4],
-//Output: 6
-//Explanation: [4,-1,2,1] has the largest sum = 6.
+//    输入: [-2,1,-3,4,-1,2,1,-5,4],
+//    输出: 6
+//    解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+//    进阶:
+//
+//    如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+//
+//    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
-class Solution{
+
+class Solution {
 public:
-    int maxSubArray(vector<int>& nums){
+    // 方法1：有些类似 dp 的思想，当 sum <= 0 时，结束当前子序列，重新计算一个新序列（贪心法？）
+    // 时间复杂度 O(n)
+    int maxSubArray(vector<int>& nums) {
         int sum = 0;
         int res = nums[0];
-        for(int i=0; i<nums.size(); i++){
-            sum =  sum>0 ? sum+nums[i] : nums[i]; //连续子序列的和，前面相加小于0，则可直接从nums[i]重新开始
-            if(sum > res){
+        for(int i = 0; i < nums.size(); i++) {
+            sum =  sum > 0 ? sum + nums[i] : nums[i]; //连续子序列的和，前面相加小于0，则可直接从nums[i]重新开始
+            if(sum > res) {
                 res = sum;
             }
         }
         return res;
     }
+    // 方法2：正规的 dp
+    // 时间复杂度 O(n)
+    int maxSubArray2(vector<int>& nums) {
+        int res = nums[0];
+        int len = nums.size();
+        vector<int> dp(len);   // dp[i] 表示 nums 中以 nums[i] 结尾的最大子序和
+        dp[0] = nums[0];
+        for (int i = 1; i < len; i++) {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
 };
-int main(){
+int main() {
     Solution s;
-    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     cout << s.maxSubArray(nums) << endl;
     return 0;
 
